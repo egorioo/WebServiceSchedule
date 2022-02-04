1a4ad355248f82ac9fda85273697a9c6e65b7b4e
egorioo <egorrudenko2002@gmail.com>
2022-02-04 19:24:13 +0200

    added basic rest api

---
+++ src/main/java/webservice/jsonUtil/ReadJson.java
@@ -0,0 +1,32 @@
+package webservice.jsonUtil;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+public class ReadJson {
+    public JSONObject readJsonFromUrl(String link) throws JSONException {
+        try (InputStream input = new URL(link).openStream()) {
+            BufferedReader re = new BufferedReader(new InputStreamReader(input, Charset.forName("UTF-8")));
+            String text = Read(re);
+            JSONObject json = new JSONObject(text);
+            return json;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public String Read(Reader re) throws IOException {
+        StringBuilder str = new StringBuilder();
+        int temp;
+        do {
+            temp = re.read();
+            str.append((char) temp);
+        } while (temp != -1);
+
+        return str.toString();
+    }
+}
